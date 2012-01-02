787e6065387001d8caca788398b197aef90f4be4
kayserg@gmail.com <kayserg@gmail.com@c3a7b540-0420-4916-8fa7-860dc1374be9>
2012-01-02 08:20:49 +0000

    Added buildroot utils modified to build miniemc2 kernel and filsystem. WEB server's spurce code is ditributed as binary package. It will be added soon as a source code

---
+++ trunk/buildroot/package/config/buildroot-config/autoconf.h
@@ -0,0 +1,89 @@
+/*
+ * Automatically generated C config: don't edit
+ * Mon Jan  2 10:15:44 2012
+ */
+#define BR2_DEBIAN_MIRROR "http://ftp.debian.org"
+#define BR2_TARGET_ROOTFS_JFFS2_EBSIZE 0x20000
+#define BR2_TARGET_ROOTFS_JFFS2_NOCLEANMARKER 1
+#define BR2_COPYTO ""
+#define BR2_BZR_CO "bzr co"
+#define BR2_TARGET_GENERIC_HOSTNAME "buildroot"
+#define BR2_TARGET_ROOTFS_TAR_GZIP 1
+#define BR2_GIT "git clone"
+#define BR2_SVN_CO "svn co"
+#define BR2_BOOST_FILESYSTEM 1
+#define BR2_TOOLCHAIN_EXTERNAL_PATH "/opt/codesourcery"
+#define BR2_BZR_UP "bzr up"
+#define BR2_KERNEL_MIRROR "http://www.kernel.org/pub/"
+#define BR2_SVN_UP "svn up"
+#define BR2_OPTIMIZE_2 1
+#define BR2_GCC_CROSS_CXX 1
+#define BR2_TARGET_GENERIC_ISSUE "Welcome to Buildroot"
+#define BR2_SOFT_FLOAT 1
+#define BR2_INSTALL_LIBSTDCPP 1
+#define BR2_GCC_TARGET_ABI "aapcs-linux"
+#define BR2_ARCH "arm"
+#define BR2_PACKAGE_BUSYBOX_FULLINSTALL 1
+#define BR2_PACKAGE_VSFTPD 1
+#define BR2_TOOLCHAIN_EXTERNAL_GLIBC 1
+#define BR2_STRIP_strip 1
+#define BR2_GNU_MIRROR "http://ftp.gnu.org/pub/gnu"
+#define BR2_VERSION "2010.05"
+#define BR2_ZCAT "gzip -d -c"
+#define BR2_PREFER_SOFT_FLOAT 1
+#define BR2_WGET "wget --passive-ftp -nd"
+#define BR2_PACKAGE_EMC2_UBOOT 1
+#define BR2_arm 1
+#define BR2_TARGET_ROOTFS_TAR 1
+#define BR2_BACKUP_SITE "http://buildroot.net/downloads/sources/"
+#define BR2_PACKAGE_BUSYBOX_SHOW_OTHERS 1
+#define BR2_STAGING_DIR "$(BASE_DIR)/staging"
+#define BR2_arm920t 1
+#define BR2_ARM_EABI 1
+#define BR2_PACKAGE_EMC2 1
+#define BR2_TOOLCHAIN_EXTERNAL_STRIP 1
+#define BR2_TARGET_MINIEMC2 1
+#define BR2_PACKAGE_XENOMAI 1
+#define BR2_TOOLCHAIN_EXTERNAL 1
+#define BR2_PACKAGE_EMC2_EMCWEB 1
+#define BR2_CONFIG_CACHE 1
+#define BR2_GCC_TARGET_ARCH "armv4t"
+#define BR2_PROGRAM_INVOCATION 1
+#define BR2_PACKAGE_EMC2_KERNEL 1
+#define BR2_TAR_OPTIONS ""
+#define BR2_TARGET_ROOTFS_JFFS2_PAGESIZE 0x800
+#define BR2_INET_RPC 1
+#define BR2_BZCAT "bzcat"
+#define BR2_PACKAGE_BUSYBOX 1
+#define BR2_PACKAGE_ZLIB 1
+#define BR2_SOURCEFORGE_MIRROR "easynews"
+#define BR2_HAVE_DOT_CONFIG 1
+#define BR2_ARM_TYPE "ARM920T"
+#define BR2_ROOTFS_POST_BUILD_SCRIPT ""
+#define BR2_LARGEFILE 1
+#define BR2_PACKAGE_BUSYBOX_CONFIG "package/busybox/busybox-1.16.x.config"
+#define BR2_GCC_TARGET_TUNE "arm920t"
+#define BR2_TOOLCHAIN_EXTERNAL_PREFIX "arm-none-linux-gnueabi"
+#define BR2_PACKAGE_EMC2_MODMINIEMC 1
+#define BR2_PTHREADS_OLD 1
+#define BR2_PACKAGE_DROPBEAR 1
+#define BR2_TARGET_ROOTFS_JFFS2_LE 1
+#define BR2_ENDIAN "LITTLE"
+#define BR2_TARGET_MINIEMC2_MINI2440 1
+#define BR2_BUSYBOX_VERSION "1.16.1"
+#define BR2_ROOTFS_DEVICE_TABLE "target/generic/device_table.txt"
+#define BR2_INET_IPV6 1
+#define BR2_ENABLE_LOCALE 1
+#define BR2_PRIMARY_SITE ""
+#define BR2_TARGET_ROOTFS_JFFS2 1
+#define BR2_BUSYBOX_VERSION_1_16_X 1
+#define BR2_USE_WCHAR 1
+#define BR2_JLEVEL 2
+#define BR2_BOOST_SERIALIZATION 1
+#define BR2_BOOST_SYSTEM 1
+#define BR2_TARGET_ROOTFS_JFFS2_NANDFLASH_2K_128K 1
+#define BR2_TARGET_OPTIMIZATION "-msoft-float -D__GCC_FLOAT_NOT_NEEDED -march=armv4 -mtune=arm920t"
+#define BR2_DL_DIR "$(TOPDIR)/dl"
+#define BR2_KERNEL_none 1
+#define BR2_PACKAGE_BOOST 1
+#define BR2_TARGET_ROOTFS_TAR_OPTIONS ""
